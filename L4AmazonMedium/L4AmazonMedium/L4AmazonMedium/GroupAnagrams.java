aa0eb88dfc1741ba877ab5eff74f7d648d7b4c46
Aamir Hussain <aamirhussainazad786@gmail.com>
2024-09-27 11:24:58 +0530

    commit add

--- L4AmazonMedium/L4AmazonMedium/L4AmazonMedium/GroupAnagrams.java
+++ L4AmazonMedium/L4AmazonMedium/L4AmazonMedium/GroupAnagrams.java
@@ -1,5 +1,36 @@
 package L4AmazonMedium;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class GroupAnagrams {
-    
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> ans = new ArrayList<>();
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            char arr[] = strs[i].toCharArray();
+            Arrays.sort(arr);
+            String s = String.valueOf(arr);
+            if (map.get(s) != null) {
+                List<String> a = map.get(s);
+                a.add(strs[i]);
+                map.put(s, a);
+            } else {
+                List<String> a = new ArrayList<>();
+                a.add(strs[i]);
+                map.put(s, a);
+            }
+        }
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            ans.add(entry.getValue());
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        String strs[] = { "eat", "tea", "tan", "ate", "nat", "bat" };
+        System.out.println(groupAnagrams(strs));
+    }
 }
