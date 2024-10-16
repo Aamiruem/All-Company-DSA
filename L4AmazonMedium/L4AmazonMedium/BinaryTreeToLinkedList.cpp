f59d81fe6ba0f42d4bfd25bb558a89509689b3b1
Aamir Hussain <aamirhussainazad786@gmail.com>
2024-10-16 16:16:27 +0530

    commit add

---
+++ L4AmazonMedium/L4AmazonMedium/BinaryTreeToLinkedList.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+// Definition for a binary tree node
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class BinaryTreeToLinkedList {
+public:
+    void flatten(TreeNode* root) {
+        // Base case
+        if (root == nullptr) {
+            return;
+        }
+        
+        // Flatten the left and right subtree recursively
+        flatten(root->left);
+        flatten(root->right);
+        
+        // Store the right subtree
+        TreeNode* tempRight = root->right;
+        
+        // Move the left subtree to the right
+        root->right = root->left;
+        root->left = nullptr; // Make sure the left subtree is set to null
+        
+        // Traverse to the end of the new right subtree
+        TreeNode* current = root;
+        while (current->right != nullptr) {
+            current = current->right;
+        }
+        
+        // Attach the previously stored right subtree
+        current->right = tempRight;
+    }
+};
+
+// Helper function to print the flattened tree (as linked list)
+void printFlattenedTree(TreeNode* root) {
+    TreeNode* current = root;
+    while (current != nullptr) {
+        cout << current->val << " -> ";
+        current = current->right;
+    }
+    cout << "null" << endl;
+}
+
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(5);
+    root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(4);
+    root->right->right = new TreeNode(6);
+    
+    BinaryTreeToLinkedList bt;
+    bt.flatten(root);
+    
+    // Output the flattened tree
+    printFlattenedTree(root);
+    
+    return 0;
+}
---
+++ L4AmazonMedium/L4AmazonMedium/minimumNoOfSwaps.java
@@ -0,0 +1,40 @@
+package L4AmazonMedium;
+
+public class minimumNoOfSwaps {
+    public static int minSwaps(String s) {
+        int count0 = 0, count1 = 0, miss0 = 0, miss1 = 0;
+
+        // Count the number of '0's and '1's
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                count0++;
+            } else {
+                count1++;
+            }
+        }
+
+        // If the difference between count of '0's and '1's is greater than 1, return -1
+        if (Math.abs(count0 - count1) > 1) {
+            return -1;
+        }
+
+        // Calculate the number of misplaced '0's and '1's
+        for (int i = 0; i < s.length(); i += 2) {
+            if (i < s.length() && s.charAt(i) != '0') {
+                miss0++;
+            }
+            if (i + 1 < s.length() && s.charAt(i + 1) != '1') {
+                miss1++;
+            }
+        }
+
+        // Return the minimum number of swaps needed
+        return count0 == count1 ? Math.min(miss0, miss1) : (count0 > count1 ? miss0 : miss1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minSwaps("111000"));  // Output: 1
+        System.out.println(minSwaps("00100"));   // Output: 0
+        System.out.println(minSwaps("111111"));  // Output: -1
+    }
+}
